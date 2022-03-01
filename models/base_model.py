2f316a20ae427d41cef11e065c33227612d6b695
Mogakamo <com02519@uoeld.ac.ke>
2022-02-28 18:34:32 -0800

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,94 +1,55 @@
 #!/usr/bin/python3
-"""
-Defines a Model class that will be the base for all the
-classes to be created in the airbnb module. This class
-will define all the basic things (attributes and functions)
-that will be used through out the project by other class
-as well
-"""
-
-from uuid import uuid4
+import uuid
 from datetime import datetime
 import models
 
 
+"""
+Base class for all models that will contain id, created_at
+and updated_at attributes. Save() and to_json() methods
+"""
+
+
 class BaseModel:
-    """A base for all the classes in the AirBnb consle project
-    Atrributes:
-        @id: string - assign with an uuid when an instance is created:
-        @created_at: datetime - assign with the current datetime when
-                     an instance is created
-        @updated_at: datetime - assign with the current datetime when
-                     an instance is created and it will be updated every
-                     time you change your object
-    Methods:
-        @__str__: should print: [<class name>] (<self.id>) <self.__dict__>
-        @save(self): updates the public instance attribute updated_at with
-                     the current datetime
-        @to_dict(self): returns a dictionary containing all keys/values of
-                        __dict__ of the instance:
-            - by using self.__dict__, only instance attributes set will be
-              returned
-            - a key __class__ must be added to this dictionary with the class
-              name of the object
-            - created_at and updated_at must be converted to string object in
-              ISO format:
-                - format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
+    """
+    Instantiation of the class BaseModel
     """
     def __init__(self, *args, **kwargs):
-        """
-        Initlizes the public attributes of the instance after creation
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-
-        TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
-
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = self.created_at
-
-        if len(kwargs) != 0:
+        """ Initializing the variables"""
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, TIME_FORMAT)
-                elif key == "id":
-                    self.__dict__[key] = str(value)
-                else:
-                    self.__dict__[key] = value
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+
         else:
+            self.id = (uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """
-        Returns the string representation of this class
-        """
+        """Method to return string representation"""
+        return ("[{}] ({}) {}".formart(str(type(self).__name__),
+                                       self.id, str(self.__dict__)))
 
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                     self.__dict__)
+    def __repr__(self):
+        """Method that returns the official representations of string"""
+        cls = self.__class__.__name__
+        string = ("[{}] [()] {}".format(cls, self.id, self.__dict__))
+        return (string)
 
     def save(self):
-        """
-        updates the updated_at public instance variable
-        """
-
-        self.updated_at = datetime.today()
+        """ Method to updaate attrb updated_at"""
+        self.update_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-            return the dictionary representation of the object
-            as stated above
-        """
+        """Method to return a dict containing all key/value of
+        __dict__ instance"""
+        dic = dict(**self.__dict__)
+        dic['__class__'] = str(type(self).__name__)
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
 
-        obj_repr = {}
-        for key, value in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                obj_repr[key] = value.isoformat()
-            else:
-                obj_repr[key] = value
-
-        obj_repr["__class__"] = self.__class__.__name__
-
-        return obj_repr
+        return (dic)
